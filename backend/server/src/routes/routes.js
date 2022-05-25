5c227b28c23a4b15c4507c852ed555089b4f51df
Ítalo Vinícius <italovinicius18@yahoo.com>
2022-05-25 09:20:27 -0300

    Sistema de login efetuado

--- backend/server/src/routes/routes.js
+++ backend/server/src/routes/routes.js
@@ -1,9 +1,37 @@
-const {Router} = require('express');
+const { Router } = require("express");
+const jwt = require("jsonwebtoken");
 
 const routes = new Router();
 
-routes.get('/status', (req, res) => {
-    res.send({message: 'Server is running'});
+routes.get("/status", (req, res) => {
+  res.send({ message: "Server is running" });
+});
+
+routes.post("/login", (req, res) => {
+  const privateKey = "@frexco-token";
+
+  console.log(req.body);
+
+  const { name, password } = req.body;
+  if (name === "admin" && password === "admin") {
+    const userData = {
+      name: "admin",
+      password: "admin",
+    };
+
+    jwt.sign(userData, privateKey, (err, token) => {
+      if (err) {
+        res.status(500).json({ mensagem: "Erro ao gerar o JWT" });
+
+        return;
+      }
+
+      res.status(200).send({"token": token});
+      res.end();
+    });
+  } else {
+    res.status(401).send({ message: "Invalid credentials" });
+  }
 });
 
 module.exports = routes;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import Router from "./routes";
 
 const App = () => <Router />;
